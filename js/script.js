38540e6b81e32021fd6f16d8355200d52705a3ea
stefanbauer <alternative2k@googlemail.com>
2021-08-04 10:32:16 +0200

    added return all character names

--- js/script.js
+++ js/script.js
@@ -26,3 +26,10 @@ const aliveCharacters = characters.filter(
   character => character.status === 'Alive'
 )
 console.log(aliveCharacters)
+
+console.clear()
+
+const allCharacterNames = characters.map(
+  characters => characters.name
+)
+console.log(allCharacterNames)
